1a801863b27cd15d6c8b295679146bb721024bec
Iltotore <rafbodaha@gmail.com>
2026-01-06 12:33:24 +0100

    feat: Add colorized ASCII-art circuit representation

--- src/io/github/iltotore/redhdl/Main.scala
+++ src/io/github/iltotore/redhdl/Main.scala
@@ -12,12 +12,20 @@ import io.github.iltotore.redhdl.graph.NetId
 import io.github.iltotore.redhdl.graph.Net
 
 object Main extends KyoApp:
-
   
   /*
   String representation of the circuit
   */
   private def showChannel(graph: Graph, layerSize: Int, layerFrom: Chunk[NodeId], channel: Channel): String =
+    val colors = Chunk(
+      scala.Console.RED,
+      scala.Console.GREEN,
+      scala.Console.BLUE,
+      scala.Console.MAGENTA,
+      scala.Console.YELLOW,
+      scala.Console.CYAN
+    )
+
     val headLine = layerFrom
       .flatMap(id => Chunk.fill(graph.getNode(id).tpe.width)(id))
       .mkString(" ")
@@ -25,42 +33,43 @@ object Main extends KyoApp:
     val width = layerSize * 2 - 1
     val height = channel.tracks.size * 4 + 1
 
-    val grid = Array.fill(height)(Array.fill(width)(' '))
+    val grid = Array.fill(height)(Array.fill(width)(" "))
 
-    def drawNet(from: Int, net: Net, id: NetId): Unit =
+    def drawNet(from: Int, net: Net, id: NetId, color: String): Unit =
       val netStartX = net.start.value * 2
       val netEndX = net.end.value * 2
       val trackZ = channel.getNetTrack(id).value * 4 + 1
+      def colored(str: String): String = s"$color$str${scala.Console.RESET}"
       
       for
         z <- from until trackZ - 1
-        if grid(z)(netStartX) == ' '
+        if grid(z)(netStartX) == " "
       do
-        grid(z)(netStartX) = '|'
+        grid(z)(netStartX) = colored("|")
 
       if netStartX == netEndX then
-        grid(trackZ - 1)(netStartX) = '|'
-        grid(trackZ)(netStartX) = '|'
-        grid(trackZ + 1)(netStartX) = '|'
+        grid(trackZ - 1)(netStartX) = colored("|")
+        grid(trackZ)(netStartX) = colored("|")
+        grid(trackZ + 1)(netStartX) = colored("|")
       else
-        grid(trackZ - 1)(netStartX) = '+'
+        grid(trackZ - 1)(netStartX) = colored("+")
         for x <- math.min(netStartX, netEndX) to math.max(netStartX, netEndX) do
-          grid(trackZ)(x) = '-'
-        grid(trackZ + 1)(netEndX) = '+'
+          grid(trackZ)(x) = colored("-")
+        grid(trackZ + 1)(netEndX) = colored("+")
 
       net.outerNet match
         case Absent =>
           for
             z <- trackZ + 2 until height
-            if grid(z)(netEndX) == ' '
-          do grid(z)(netEndX) = '|'
+            if grid(z)(netEndX) == " "
+          do grid(z)(netEndX) = colored("|")
         
         case Present(outerId) =>
-          drawNet(trackZ + 2, channel.getNet(outerId), outerId)
+          drawNet(trackZ + 2, channel.getNet(outerId), outerId, color)
     end drawNet
 
     for (net, id) <- channel.nets.zipWithIndex if !channel.isOuterColumn(net.start) do
-      drawNet(0, net, NetId.assume(id))
+      drawNet(0, net, NetId.assume(id), colors(id % colors.size))
     
     s"$headLine\n${grid.map(_.mkString).mkString("\n")}"
 
