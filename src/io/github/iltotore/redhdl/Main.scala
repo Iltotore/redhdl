0dbcae0ba2b607f3f1b951145b04fc8f5b7c78f1
Iltotore <rafbodaha@gmail.com>
2026-02-05 11:45:53 +0100

    fix: Structure Z dim calculation

--- src/io/github/iltotore/redhdl/Main.scala
+++ src/io/github/iltotore/redhdl/Main.scala
@@ -92,7 +92,7 @@ object Main extends KyoApp:
 
   run:
     direct:
-      val code = Using.resource(Source.fromFile("test/resources/golden/good/biIdentityDeep.red"))(_.mkString)
+      val code = Using.resource(Source.fromFile("test/resources/golden/good/circuit/twoBitAdder.red"))(_.mkString)
 
       val typeResult = typecheck(code)
       Console.printLine(typeResult).now
@@ -100,7 +100,7 @@ object Main extends KyoApp:
 
       typeResult match
         case Result.Success(components) =>
-          val initialGraph = compileToGraph(Identifier("BiIdentityDeep"), components)
+          val initialGraph = compileToGraph(Identifier("TwoBitAdder"), components)
           val initialLayers = GraphRouter.getLayers(initialGraph)
           val (graph, layers) = GraphRouter.addRelays(initialGraph, initialLayers)
           Console.printLine(graph).now
